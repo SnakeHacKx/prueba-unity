f1545ceae523386c67067f389fbf2be36c619f22
Omar Alexis <omaralexisxdx2@gmail.com>
2021-01-13 20:00:01 -0500

    Primer commit en la prueba

---
+++ Assets/Scripts/Nose.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nose : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("PRUEBA DE UNITY CON GIT Y GITHUB");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
